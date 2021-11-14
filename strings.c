6729fbc61fb80c78e419ad9a94cc4575ebeea3e0
niraj <nirajbava222@gmail.com>
2021-11-14 17:36:29 +0530

    add new code

---
+++ strings.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void printstrig(char string[])
+{
+    int i= 0;
+    while(string[i]!='\0')
+    {
+        printf("%c", string[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+int main() 
+{
+    // char str[] = {'n','i','r','a','j','\0'};
+    char str[10]; // =  "niraj";                  
+    gets(str);
+    printstrig(str);
+    printf("using printf function : %s\n", str);
+    printf("using puts : ");
+    puts(str);
+    return 0;
+}
